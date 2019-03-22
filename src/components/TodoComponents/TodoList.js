87266d7cbf4d4e2e73a0fa376ee76606e060130c
Adetunji <ashennaike@gmail.com>
2019-03-21 22:24:35 -0400

    working local storage

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -7,19 +7,27 @@ import ToDoForm from './TodoForm'
 import ListItem from './Todo'
 import './TodoList.css'
 
+let stored = window.localStorage
 
-let taskList = [
-  {
-    task: 'Example',
-    id: 123456789,
-    completed: false
-  },
-  {
-    task: 'Now You Try',
-    id: 987654321,
-    completed: false
-  }
-]
+let taskList
+
+if (JSON.parse(stored.getItem('exchange'))){
+  taskList = JSON.parse(stored.getItem('exchange'))
+}
+else{
+  taskList = [
+    {
+      task: 'Example',
+      id: 123456789,
+      completed: false
+    },
+    {
+      task: 'Now You Try',
+      id: 987654321,
+      completed: false
+    }
+  ]
+}
 
 class TodoList extends React.Component {
   constructor() {
@@ -52,8 +60,11 @@ class TodoList extends React.Component {
     
     exchange.push(newItem);
 
+    stored.setItem('exchange', JSON.stringify(exchange))
+
     this.setState({
-      taskList: exchange,
+      taskList: JSON.parse(stored.getItem('exchange')),
+      // taskList: exchange,
       task:''
     });
   };
@@ -79,32 +90,41 @@ class TodoList extends React.Component {
     let exchange = this.state.taskList.filter(task => 
       task.completed === false      
     )
- 
+
+    stored.setItem('exchange', JSON.stringify(exchange))
+
     this.setState({
-      taskList: exchange
+      taskList: JSON.parse(stored.getItem('exchange')),
     })
 
   }
 
   render() {
+    console.log(taskList)
     return (
-      <div>
-        <h1>To Do List Below:</h1>
-        <div className='list'>
-          {this.state.taskList.map((toDoList) => (
-            <ListItem key={toDoList.id} toDoProp={toDoList} toggleCompleted={this.toggleCompleted} />
-          ))} 
+      <div className='container'>
+        <div className='search'>
+        
         </div>
+        <div className='list-holder'>
+          <h1>To Do List Below:</h1>
+          <div className='list'>
+            {this.state.taskList.map((toDoList) => (
+              <ListItem key={toDoList.id} toDoProp={toDoList} toggleCompleted={this.toggleCompleted} />
+            ))} 
+          </div>
 
-        <ToDoForm
-          task={this.state.task}
-          id={this.state.id}
-          completed={this.state.completed}
-          eventChanges={this.eventChanges}
-          // toggleCompleted={this.toggleCompleted}
-          updateList={this.updateList}
-          removeComplete={this.removeComplete}
-        />
+          <ToDoForm
+            task={this.state.task}
+            id={this.state.id}
+            completed={this.state.completed}
+            eventChanges={this.eventChanges}
+            // toggleCompleted={this.toggleCompleted}
+            updateList={this.updateList}
+            removeComplete={this.removeComplete}
+            className='form'
+          />
+        </div>
       </div>
     )
   }
